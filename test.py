233ed2560da1350a61ec4af41b21e7bd2034465e
willclarktech <willclarktech@users.noreply.github.com>
2020-10-22 18:15:21 +0200

    Add test script

---
+++ test.py
@@ -0,0 +1,39 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms  # type: ignore
+from typing import List
+
+from train import Net, target_transform
+
+
+def main() -> None:
+    transform = transforms.Compose([transforms.Grayscale(), transforms.ToTensor()])
+    datadir = "./data/grayscale-digits-test"
+    folder_names = sorted(os.listdir(datadir))
+    dataset = datasets.ImageFolder(
+        datadir, transform=transform, target_transform=target_transform(folder_names)
+    )
+    batch_size = 1000
+    test_loader = DataLoader(dataset, batch_size=batch_size)
+
+    model = Net()
+    state_dict = torch.load("./models/grayscale-digits.pkl")
+    model.load_state_dict(state_dict)
+    model.eval()
+
+    score = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            predictions = output.argmax(2, keepdim=True)
+            correct = predictions.eq(target.view_as(predictions)).sum(1).flatten()
+            score += correct.eq(6).sum().item()
+
+    print(
+        f"Correct: {score}/{len(test_loader.dataset)} ({100 * score/len(test_loader.dataset)}%)"
+    )
+
+
+if __name__ == "__main__":
+    main()
